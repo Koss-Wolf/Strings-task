68c349501a62cc78f11c135fa3d287caccac289d
Kostia Vovk <kosswolf47@gmail.com>
2023-07-03 22:21:46 +0300

    Add code to task1

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,4 +1,29 @@
 package task1;
 
 public class Main {
+    private final static char CORRECT = 'o', WRONG = 'b';
+
+    public static void main(String[] args) {
+        print(string("brange", "plum", "tomato", "onibn", "grape"));
+    }
+
+
+    private static String[] string(String... array) {
+        int length = array.length;
+        String[] correct = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            correct[i] = array[i].replace(WRONG, CORRECT);
+        }
+        return correct;
+    }
+
+    private static void print(String... array) {
+        int i = 1;
+
+        for (String str : array) {
+            System.out.println(i + " " + str);
+            i++;
+        }
+    }
 }
